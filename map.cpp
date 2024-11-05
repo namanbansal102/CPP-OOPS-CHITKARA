f97388270e3e8eb31caea67ef0226d4fd0a933c1
Naman Bansal <namanbansal102@gmail.com>
2024-11-05 17:08:21 +0530

    Map in cpp

--- map.cpp
+++ map.cpp
@@ -1,6 +1,20 @@
 #include<iostream>
+#include<map>
 using namespace std;
 int main() {
-    
+    map<int,string> mawp;
+    mawp[1]="Naman";
+    mawp[2]="Bansal";
+    mawp[3]="Naman1";
+    mawp[4]="Naman2";
+    mawp[5]="Naman3";
+   map<int,string>::iterator it=mawp.begin();
+   while (it!=mawp.end())
+   {
+    cout<<it->first<<endl;
+    cout<<it->second<<endl;
+    ++it;
+   }
+   
     return 0;
 }
